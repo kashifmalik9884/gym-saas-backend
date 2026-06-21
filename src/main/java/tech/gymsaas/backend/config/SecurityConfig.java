495df92a45278b28e09513741fb8a4cf9c8dff0b
MOHD KASHIF <76276260+kashifmalik9884@users.noreply.github.com>
2026-06-21 14:15:26 +0530

    Update SecurityConfig.java

--- src/main/java/tech/gymsaas/backend/config/SecurityConfig.java
+++ src/main/java/tech/gymsaas/backend/config/SecurityConfig.java
@@ -119,7 +119,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://localhost:3000",
-                "https://your-frontend-domain.com"
+                "https://https://frontend-ahgr.onrender.com"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
