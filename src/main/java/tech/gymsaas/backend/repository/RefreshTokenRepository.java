9ce0d2356da9a28f20cd55bb78be03e481fdaef8
MOHD KASHIF <76276260+kashifmalik9884@users.noreply.github.com>
2026-07-14 10:37:50 +0530

    Update RefreshTokenRepository.java

--- src/main/java/tech/gymsaas/backend/repository/RefreshTokenRepository.java
+++ src/main/java/tech/gymsaas/backend/repository/RefreshTokenRepository.java
@@ -13,4 +13,5 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
     Optional<RefreshToken> findByUser(User user);
 
     void deleteByUser(User user);
+     void deleteByUser_Id(Long userId);
 }
